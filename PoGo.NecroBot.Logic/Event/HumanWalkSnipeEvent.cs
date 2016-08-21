9de3706e78a90813f0f8be633cefe1d561863877
Truong Nguyen <samuraitruong@hotmail.com>
2016-08-21 23:10:40 +1000

    move message to event listener

---
+++ PoGo.NecroBot.Logic/Event/HumanWalkSnipeEvent.cs
@@ -0,0 +1,33 @@
+﻿#region using directives
+
+using System.Collections.Generic;
+using PoGo.NecroBot.Logic.Model;
+using POGOProtos.Enums;
+using Location = PoGo.NecroBot.Logic.Model.Settings.Location;
+
+#endregion
+
+namespace PoGo.NecroBot.Logic.Event
+{
+    public enum HumanWalkSnipeEventTypes
+    {
+        StartWalking,
+        DestinationReached,
+        PokemonScanned
+    }
+    public class HumanWalkSnipeEvent : IEvent
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+        public double Distance { get; set; }
+
+        public double WalkTimes { get; set; }
+
+        public PokemonId PokemonId { get; set; }
+        public HumanWalkSnipeEventTypes Type { get; set; }
+        public double Expires { get; internal set; }
+        public int Estimate { get; internal set; }
+        public List<string> RarePokemons { get; internal set; }
+    }
+}
